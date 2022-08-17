3ed16b934e36b719947f2f364ad96fba4e3bcd17
Dhiraj Mahadik <90106820+DhirajMahadik@users.noreply.github.com>
2022-08-18 04:02:50 +0530

    Update Skilss.js

--- src/Components/Skilss.js
+++ src/Components/Skilss.js
@@ -278,8 +278,7 @@ export default function Skliss() {
                         <h2>About <samp>Me</samp></h2>
                         <h5>Developer <span>& Designer</span></h5>
                         <p>I am a front-end web developer. I can provide clean code and pixel perfect design. I also make the
-                            website more & more interactive with web animations.I can provide clean code and pixel perfect
-                            design. I also make the website more & more interactive with web animations.A responsive design
+                            website more & more interactive with web animations. A responsive design
                             makes your website accessible to all users, regardless of their device.</p>
                         
                             <a href="https://api.whatsapp.com/send/?phone=919284593357&text&app_absent=0" target="_blank" rel="noreferrer">
