92919d92fe30fdd25363c98a1bf79fdff0d9e27a
Dhiraj <dhirajmahadik9221@gmail.com>
2023-01-24 22:10:02 +0530

    NA

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
 
   const Header = styled.nav`
 
-  nav{
+  /* nav{
     background-color: black;
     
     .navbar-brand{
@@ -22,15 +22,15 @@ export default function Navbar(props) {
       }
     }
 
-    .collapse{
-      /* justify-content: end; */
+  
+     
 
       .nav-link{
         color: #fff;
       }
-    }
+    
 
-  }
+  } */
 
   `;
 
@@ -42,10 +42,10 @@ export default function Navbar(props) {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link "  to="/">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/skills">skills</Link>
